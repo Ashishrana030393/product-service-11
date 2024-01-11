abbf7a1f780f23f09f5a73a7ab3583f1030f1ff8
kamal <kamal@onelap.in>
2024-01-11 12:26:02 +0530

    java 11 springboot

---
+++ src/main/java/com/djanog/productservice11/ProductService11Application.java
@@ -0,0 +1,13 @@
+package com.djanog.productservice11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductService11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductService11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/djanog/productservice11/ProductService11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djanog.productservice11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductService11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
